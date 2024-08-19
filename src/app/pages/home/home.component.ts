809c3be8f9790b8a39cc66b93ce8a52c07e4c80d
Joel Razuri <jrazuri98@hotmail.com>
2024-08-18 23:09:30 -0300

    create methods for navigation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  constructor(private readonly route: Router) {}
 
+  navigateToLogin() {
+    this.route.navigate(['/login']);
+  }
+
+  navigateToSignUp() {
+    this.route.navigate(['/sign-up']);
+  }
 }
